a52d65dca9306ac6a6525c34179a9668e9f19a47
giset <giset@Gis12di>
2024-10-23 00:48:57 -0500

    Interfaz admin

---
+++ src/apexconstructors/ApexConstructors.java
@@ -0,0 +1,10 @@
+
+import javax.swing.SwingUtilities;
+
+public class ApexConstructors {
+
+    public static void main(String[] args) {
+        // Ejecutar la interfaz de Login
+        SwingUtilities.invokeLater(() -> new Login().setVisible(true));
+    }
+}
