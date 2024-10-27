aea0a296b2b9ff24b7670280e7e54e459d904c9f
DavidB0801 <jdavidbravo2004@gmail.com>
2024-10-26 23:21:41 -0500

    cambio de paquete o de capa para la clase Conexion

--- src/Persistencia/CrudTorres.java
+++ src/Persistencia/CrudTorres.java
@@ -2,7 +2,6 @@ package Persistencia;
 
 // CrudTorres.java
 import Logica.Torre.Torre;
-import Persistencia.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
