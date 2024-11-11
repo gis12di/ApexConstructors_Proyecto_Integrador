c51c2ba70b3a3d9edf670a7d5517cfab1800fd4d
gis12di <gisethasdisu612@gmail.com>
2024-11-11 16:25:00 -0500

    MODIFICACION MAIN_ASESOR

--- src/Visuales/Main_Asesor.java
+++ src/Visuales/Main_Asesor.java
@@ -5,8 +5,6 @@ import Logica.Torre.Torre;
 import Persistencia.CrudTorres;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.border.EmptyBorder;
 
